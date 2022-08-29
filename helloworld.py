928dea32cf1422fe9e0e45849b670d5836590bb3
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-29 11:02:53 -0300

    ++ data types

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,29 @@
-print("hello world")
+# Comentario
 
+# Tipos de datos py
+
+# Strings 
+
+print('hello world')
+#integer
+print(30) 
+#float
+print(30.4)
+# boolean 
+
+True 
+False 
+
+# List 
+
+[10, 20, 30, 55]
+
+[10, 20, 30, 44.4, True, 'cocacola']
+
+[] # lista vacia
+
+# Tuples (son listas pero constantes)
+
+(10, 20, 30)
+
+() #tupla vacia
