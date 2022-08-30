e79052ab4b24cc01c92241efbd634e4a781a5c2c
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-30 12:46:35 -0300

    loops

--- condi.py
+++ condi.py
@@ -8,13 +8,18 @@
 # else:
 #     print("x is greater than 20")
 
-name = "Mark"
-lastname = "Carter"
+# name = "Mark"
+# lastname = "Carter"
 
-if name == "John":
-    if lastname == "Carter":
-        print("You are John Carter")
-    else:
-        print("You aren't Jhon")
-else:
-    print("You are Mark")
+# if name == "John":
+#     if lastname == "Carter":
+#         print("You are John Carter")
+#     else:
+#         print("You aren't Jhon")
+# else:
+#     print("You are Mark")
+
+# if x > 10 and y > 5:
+#     print()
+
+
---
+++ loops.py
@@ -0,0 +1,6 @@
+foods = ['apples', 'bread', 'cocacola', 'leche']
+
+for food in foods:
+    if food == "leche":
+        print("you must buy milk...")
+    print(food)
